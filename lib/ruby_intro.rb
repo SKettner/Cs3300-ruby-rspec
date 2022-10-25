d9b5afb720ac9ef770051ae3383d39c847b2dff2
SKettner <skettner@uccs.edu>
2022-10-25 15:00:07 +0000

    Finshed coding part 3 and fixed up the last problum on part 1.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -11,36 +11,100 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  _temp = arr
-  arr.each{ |x|
-  _temp.delete_at(0)
-    
-    _temp.each{ |y| 
-      if n = (x*y)
-      return true
+ 
+  for x in (0..(arr.length()-1))
+
+    for y in (0..(arr.length()-1))
+
+      if x==y
+
+      elsif n == (arr[x]+arr[y])
+
+        return true
+
       end
-    }
-    _temp.push(x)
-  }
-  return false
+
+    end
+
+   end
+
+   return false
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  temp = "Hello, "
+  temp += (name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    s = "a"
+  end
+  if s[0].downcase.match(/b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z/)
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  temp = s.to_i
+  if s.empty?
+    s = "a"
+  end
+  if(temp % 4  == 0 && !(/[a-zA-Z^$2-9*]/.match(s)))
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  @@isbn = 0
+  @@price = 0
+
+  def initialize(isbn, price)
+
+    @@isbn = isbn
+    @@price = price
+
+    raise ArgumentError.new(
+      "Price can not be negitive"
+    ) if price <= 0
+
+    raise ArgumentError.new(
+      "ISPN can not be null"
+    ) if isbn == ''
+
+  end 
+
+  def isbn
+    @@isbn
+  end
+
+  def isbn=(isbn)
+    @@isbn = isbn
+  end
+
+  def price
+    @@price
+  end
+
+  def price=(price)
+    @@price = price
+  end
+
+  def price_as_string
+  
+    string_price = "%.2f" % @@price
+    string_price.prepend("$") 
+    return string_price
+
+  end 
+  
+
 end
